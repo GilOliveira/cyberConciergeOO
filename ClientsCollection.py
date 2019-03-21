d44afaa57db351364d0711e3c56b184d6815238e
Gil Oliveira <mariogil@edu.ulisboa.pt>
2019-03-21 16:23:03 +0000

    Created and started working on ClientsCollection.

---
+++ ClientsCollection.py
@@ -0,0 +1,51 @@
+# 2018-2019 Programação 2 (LTI)
+# Grupo 34
+# 49269 Mário Gil Oliveira
+# 46261 Margarida Rolo
+
+from Client import Client
+from copy import deepcopy
+
+
+class ClientsCollection:
+
+    def __init__(self):
+        """
+        Creates a new ClientsCollection object,
+        a collection of Client.
+        The collection is created empty.
+        """
+
+        self._data = []
+
+    def getClientsList(self):
+        """
+        Gets the list of clients
+        Ensures: a List of clients
+        """
+
+        return deepcopy(self._data())
+
+    def addClient(self, client):
+        """
+        Adds a new client to the collection.
+        Requires: client is Client
+        """
+        self._data.append(client)
+
+    def items(self):
+        """
+        Iterates on each of the clients.
+        """
+
+        for i in self.getClientsList():
+            yield i
+
+    def sortByRequestTime(self):
+        """
+        Rearranges the collection in order of
+        request time.
+        """
+        pass
+
+
