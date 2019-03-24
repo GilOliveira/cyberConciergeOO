beed6e72f8fe72f6d416b96d5d344ea94a9c9594
Gil Oliveira <giloliveira@outlook.com>
2019-03-24 21:39:11 +0000

    Added the scheduling module

---
+++ scheduling.py
@@ -0,0 +1,10 @@
+# 2018-2019 Programação 2 (LTI)
+# Grupo 34
+# 49269 Mário Gil Oliveira
+# 46261 Margarida Rolo
+
+def update:
+    """
+    Função obrigatória tendo em conta a especificação
+    """
+    pass
