6aa3640e463cd62fb964d485b23fd4abf2599e9d
Gil Oliveira <giloliveira@outlook.com>
2019-03-27 16:01:51 +0000

    much of the assign function is now done

--- cyberConc.py
+++ cyberConc.py
@@ -4,4 +4,33 @@
 # 46261 Margarida Rolo
 
 import sys
+from ExpertsCollection import ExpertsCollection
+from ClientsCollection import ClientsCollection
+import scheduling
+import filesWriting
+from dateTime import dateTime
 
+def assign(inputExperts, inputClients):
+
+    # COLOCAR CÓDIGO QUE CHAME O FILES READING AQUI
+    # OS INPUTS DEVERÃO SER INSERIDOS EM inRequests e inExperts
+    # TAMBÉM É NECESSÁRIO TER O scheduleTime e o company
+
+    outScheduling = scheduling.update(inRequests, inExperts)
+
+    newSchedule = outScheduling[0]
+    newExperts = outScheduling[1]
+
+    scheduleTime.addTime(30)
+    scheduleName = filesWriting.newFile(scheduleTime, 'schedule', company)
+    filesWriting.writeSchedule(scheduleName, newSchedule)
+
+    expertsName = filesWriting.newFile(scheduleTime, 'experts', company)
+    filesWriting.writeExperts(expertsName, newExperts)
+
+# ---- PROGRAM STARTS RUNNING HERE -------
+
+inputExperts, inputClients = sys.argv[1:]
+
+if True:  # SUBSTITUIR COM CHECK ERROS
+    assign(inputExperts, inputClients)
